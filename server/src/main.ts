f33b911bd21a1137f1bd19f3dc5c508e7ad84155
Huy <huy.truong@devsamurai.com>
2024-03-02 14:46:47 +0700

    AN-BE1 feat: init prisma, containerized Postgres DB, init SwaggerUI

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // const app = initializeApp(firebaseConfig);
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'jwt',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'jwt', // This name should match the security name defined below
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //un comment this line to enable global validation JWT
+  // app.useGlobalPipes(new ValidationPipe());
+  await app.listen(4000);
 }
 bootstrap();
