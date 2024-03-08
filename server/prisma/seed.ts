c713d5b80c12d4423ae243b5fee9aa329afcd4c9
UnDeadCharger <rexfury121@gmail.com>
2024-03-09 02:39:42 +0700

    KAN-DB feat: schema and migration

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -7,31 +7,6 @@ const prisma = new PrismaClient();
 
 async function main() {
   // create two dummy articles
-  const task1 = await prisma.task.upsert({
-    where: { title: 'Design Database Schema' },
-    update: {},
-    create: {
-      title: 'Design Database Schema',
-      estimatedHours: 4,
-      assigneeId: 1,
-      assigneeName: 'HuyGia',
-      status: 'In Progress',
-    },
-  });
-
-  const task2 = await prisma.task.upsert({
-    where: { title: 'Create API for demo' },
-    update: {},
-    create: {
-      title: 'Create API for demo',
-      estimatedHours: 4,
-      assigneeId: 1,
-      assigneeName: 'HuyGia',
-      status: 'In Progress',
-    },
-  });
-
-  console.log({ task1, task2 });
 }
 
 // execute the main function
