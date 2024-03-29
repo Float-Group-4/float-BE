324a1b64c976573af834fde0fd464e962d309982
nkhang1902 <nkhang1902@gmail.com>
2024-03-29 11:25:39 +0700

    Init Mailer Service

---
+++ mailer/src/mail/mail.service.ts
@@ -0,0 +1,22 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  async sendUserConfirmation(email: string, name: string, token: string) {
+    const url = `example.com/auth/confirm?token=${token}`;
+
+    await this.mailerService.sendMail({
+      to: email,
+      // from: '"Support Team" <support@example.com>', // override default from
+      subject: 'Welcome to Nice App! Confirm your Email',
+      template: './confirmation',
+      context: {
+        name: name,
+        url,
+      },
+    });
+  }
+}
